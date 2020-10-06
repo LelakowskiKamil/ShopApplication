08a2bb4978e6731274de9e2d1e8bc59ffbc50dc0
Kamil Lelakowski <kamilel197@gmail.com>
2020-10-06 23:19:02 +0200

    adding missing CRUD methods to customer/product/cart Controller class

--- src/main/java/com/example/REST/service/expection/CartNotFoundException.java
+++ src/main/java/com/example/REST/service/expection/CartNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.REST.service.expection;
 
 public class CartNotFoundException extends RuntimeException {
 
-    CartNotFoundException(Long id){
-        super("Couldn't find order "+id);
+    public CartNotFoundException(Long id){
+        super("Couldn't find cart "+id);
     }
 }
